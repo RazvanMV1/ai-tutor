3ecdb62613962b0c88c27f78dbcfca2cab85fe87
RazvanMV1 <156513208+RazvanMV1@users.noreply.github.com>
2026-03-09 18:06:30 +0200

    Delete src/frontend/AiTutor.Web directory

--- src/frontend/AiTutor.Web/Store/Progress/ProgressActions.cs
+++
@@ -1,7 +0,0 @@
-using AiTutor.Web.Models;
-
-namespace AiTutor.Web.Store.Progress;
-
-public record LoadProgressAction(Guid UserId);
-public record LoadProgressSuccessAction(List<StudentProgressDto> Items);
-public record LoadProgressFailureAction(string Error);
--- src/frontend/AiTutor.Web/Store/Subjects/SubjectsActions.cs
+++
@@ -1,8 +0,0 @@
-using AiTutor.Web.Models;
-
-namespace AiTutor.Web.Store.Subjects;
-
-public record LoadSubjectsAction;
-public record LoadSubjectsSuccessAction(List<SubjectDto> Subjects);
-public record LoadSubjectsFailureAction(string Error);
-public record LoadLessonsSuccessAction(List<LessonDto> Lessons);
